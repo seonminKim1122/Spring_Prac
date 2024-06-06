3846fb382db77f715a66dee49318c052ef30f256
Kim Seonmin <w.hand715@gmail.com>
2024-06-06 11:23:31 +0900

    feat : ReplyRepository 추가

---
+++ demo/src/main/java/com/example/demo/reply/repository/ReplyRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.reply.repository;
+
+import com.example.demo.reply.domain.Reply;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReplyRepository extends CrudRepository<Reply, Long> {
+}
